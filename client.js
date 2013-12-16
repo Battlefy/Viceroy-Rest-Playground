e56b2c3b1773c63a83fda66de87d6b161268ed13
shanejonas <jonas.shane@gmail.com>
2013-12-15 23:16:02 -0800

    add comments to client.js

--- client.js
+++ client.js
@@ -15,7 +15,9 @@ viceroy.driver(viceroyRest({
 viceroy.connect(function(){
 
   var person = new Person({name: 'Shane'});
+  // POST http://localhost:8000/people
   person.save(function(err, person){
+    // GET http://localhost:8000/people?name=Shane
     Person.find({name: 'Shane'}, function(err, people){
       people.forEach(function(person){
         document.body.appendChild(personView(person.data()));
